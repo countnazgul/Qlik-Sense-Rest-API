4184fdf346bf9dc2de9258c514538a99fe2ad3a6
Stefan Stoichev <stefan.stoichev@gmail.com>
2021-02-22 10:23:06 +0000

    responseType in all child classes
    #closes 4

--- src/clients/Generic.ts
+++ src/clients/Generic.ts
@@ -1,4 +1,5 @@
 import { IConfig, IConfigFull, IHttpReturn } from "../interfaces/interfaces";
+import { ResponseType } from "axios";
 import { QlikClient } from "./BaseClient";
 
 export class QlikGenericRestClient extends QlikClient {
@@ -15,35 +16,69 @@ export class QlikGenericRestClient extends QlikClient {
     super(configFull);
   }
 
-  Get(path: string, contentType = "application/json"): Promise<IHttpReturn> {
-    return super.Get(`${this.configFull.baseUrl}/${path}`, contentType);
+  Get(
+    path: string,
+    contentType = "application/json",
+    responseType?: ResponseType
+  ): Promise<IHttpReturn> {
+    return super.Get(
+      `${this.configFull.baseUrl}/${path}`,
+      contentType,
+      responseType
+    );
   }
 
-  Delete(path: string, contentType = "application/json") {
-    return super.Delete(`${this.configFull.baseUrl}/${path}`, contentType);
+  Delete(
+    path: string,
+    contentType = "application/json",
+    responseType?: ResponseType
+  ) {
+    return super.Delete(
+      `${this.configFull.baseUrl}/${path}`,
+      contentType,
+      responseType
+    );
   }
 
   Patch(
     path: string,
     data: object,
-    contentType = "application/json"
+    contentType = "application/json",
+    responseType?: ResponseType
   ): Promise<IHttpReturn> {
-    return super.Post(`${this.configFull.baseUrl}/${path}`, data, contentType);
+    return super.Post(
+      `${this.configFull.baseUrl}/${path}`,
+      data,
+      contentType,
+      responseType
+    );
   }
 
   Post(
     path: string,
     data: object,
-    contentType = "application/json"
+    contentType = "application/json",
+    responseType?: ResponseType
   ): Promise<IHttpReturn> {
-    return super.Post(`${this.configFull.baseUrl}/${path}`, data, contentType);
+    return super.Post(
+      `${this.configFull.baseUrl}/${path}`,
+      data,
+      contentType,
+      responseType
+    );
   }
 
   Put(
     path: string,
     data: object,
-    contentType = "application/json"
+    contentType = "application/json",
+    responseType?: ResponseType
   ): Promise<IHttpReturn> {
-    return super.Put(`${this.configFull.baseUrl}/${path}`, data, contentType);
+    return super.Put(
+      `${this.configFull.baseUrl}/${path}`,
+      data,
+      contentType,
+      responseType
+    );
   }
 }
